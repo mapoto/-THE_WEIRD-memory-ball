ed16b95d7c2c57b3ae044e68b2fc242b753eb233
gejo4207 <lucky.chandrautama@uni-weimar.de>
2019-06-13 15:02:18 +0200

    First Sync

---
+++ Assets/Scripts/Camera Effect/UnsyncCameraEffect.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class UnsyncShader : MonoBehaviour
+{
+    public float speed = 1;
+    private float position = 0;
+    private Material material;
+
+    void Awake()
+    {
+        material = new Material(Shader.Find("Hidden/VUnsync"));
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        position += speed * 0.1f * Time.deltaTime;
+        position = position%1f;
+        material.SetFloat("_ValueX", position);
+
+        RenderTexture renderTexture = RenderTexture.GetTemporary(source.width, source.height, 0, source.format);
+
+        Graphics.Blit(source, renderTexture, material);
+        Graphics.Blit(renderTexture, destination, material);
+        RenderTexture.ReleaseTemporary(renderTexture);
+
+    }
+}
---
+++ Assets/Scripts/SkyBoxControllerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBoxControllerScript : MonoBehaviour
+{
+
+    public Material[] materials;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeTheSkybox();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChangeTheSkybox()
+    {
+        int random = Random.Range(0, materials.Length - 1);
+        RenderSettings.skybox = materials[random];
+    }
+}
