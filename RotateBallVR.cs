31d28ed9640eb77d97b3f2564fa530858e55a8b9
Lucky Chandrautama <38219434+mapoto@users.noreply.github.com>
2019-06-20 19:48:43 +0200

    rotation using vr is added

---
+++ RotateBallVR.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class RotateBallVR : MonoBehaviour
+{
+    public SteamVR_Behaviour_Pose vR_Input;
+    public static float h;
+    public static float v;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        h = vR_Input.transform.rotation.y;
+        v = vR_Input.transform.rotation.x;
+
+        Debug.Log(h);
+
+        transform.Rotate(v, h, 0);
+    }
+
+}
